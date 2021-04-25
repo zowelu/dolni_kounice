629e4af10be4a9c28598b1331f81fc5dde0f2d8e
zoublu <zoublu@email.cz>
2021-04-26 01:30:51 +0200

    V home_page.dart předělán scaffold na defaultPageOfChoice

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mesto_pamatek/components/default_page_of_choice.dart';
 import 'package:mesto_pamatek/components/my_app_bar.dart';
 import 'package:mesto_pamatek/constants/constants.dart';
 import 'package:mesto_pamatek/components/choice_container.dart';
@@ -14,61 +15,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Město památek',
-      home: Scaffold(
-        backgroundColor: kBackgroundColor,
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(kDefaultMyAppBarHeight),
-          child: MyAppBar(
-            tittleOfAppbar: 'Město památek',
-            onPressedBackButton: () {
-              Navigator.pop(context);
-            },
+    return DefaultPageOfChoice(
+      tittleOfAppbar: 'Město památek',
+      titleOfHeaderText: '',
+      listOfWidget: [
+        ContainerHeaderHomePage(),
+        Container(
+          padding:
+              EdgeInsets.only(left: kDefaultPadding, right: kDefaultPadding),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ChoiceContainer(
+                  assetImageOfChoice:
+                      'assets/images/pamatky/klaster_rosa_coeli/klaster_krizova_chodba.jpg',
+                  textOfChoice: 'Audioprůvodce',
+                  onTap: () {
+                    Navigator.pushNamed(context, AudioGuide.id);
+                  }),
+              ChoiceContainer(
+                  assetImageOfChoice:
+                      'assets/images/pamatky/mesto_pamatek_uvod/pamatky-mapa.jpg',
+                  textOfChoice: 'Památky ve městě',
+                  onTap: () {
+                    Navigator.pushNamed(context, Monuments.id);
+                  }),
+              ChoiceContainer(
+                  assetImageOfChoice:
+                      'assets/images/pamatky/mesto_pamatek_uvod/beautiful-scenery-greenfield-countryside-eifel-region-germany-M.jpg',
+                  textOfChoice: 'Tipy na výlety v okolí',
+                  onTap: () {}),
+            ],
           ),
         ),
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                ContainerHeaderHomePage(),
-                Container(
-                  padding: EdgeInsets.only(
-                      left: kDefaultPadding, right: kDefaultPadding),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ChoiceContainer(
-                          assetImageOfChoice:
-                              'assets/images/pamatky/klaster_rosa_coeli/klaster_krizova_chodba.jpg',
-                          textOfChoice: 'Audioprůvodce',
-                          onTap: () {
-                            Navigator.pushNamed(context, AudioGuide.id);
-                          }),
-                      ChoiceContainer(
-                          assetImageOfChoice:
-                              'assets/images/pamatky/mesto_pamatek_uvod/pamatky-mapa.jpg',
-                          textOfChoice: 'Památky ve městě',
-                          onTap: () {
-                            Navigator.pushNamed(context, Monuments.id);
-                          }),
-                      ChoiceContainer(
-                          assetImageOfChoice:
-                              'assets/images/pamatky/mesto_pamatek_uvod/beautiful-scenery-greenfield-countryside-eifel-region-germany-M.jpg',
-                          textOfChoice: 'Tipy na výlety v okolí',
-                          onTap: () {}),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      ],
     );
   }
 }
