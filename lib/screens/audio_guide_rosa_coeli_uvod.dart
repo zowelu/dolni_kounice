0ce765ae85b34005126eb50e2cfdaa2d8f9bcf99
zoublu <zoublu@email.cz>
2021-04-25 23:50:24 +0200

    Vytvořen static String id pro audio_guide_rosa_coeli_uvod.dart.dart.dart.dart a upraven routes v main.dart

--- lib/screens/audio_guide_rosa_coeli_uvod.dart
+++ lib/screens/audio_guide_rosa_coeli_uvod.dart
@@ -3,6 +3,7 @@ import 'package:mesto_pamatek/components/rosa_coeli_map_of_path_and_text.dart';
 import 'package:mesto_pamatek/components/audio_page.dart';
 
 class AudioGuideRosaCoeliUvod extends StatefulWidget {
+  static String id = '/audioGuideRosaCoeliUvod';
   @override
   _AudioGuideRosaCoeliUvodState createState() =>
       _AudioGuideRosaCoeliUvodState();
