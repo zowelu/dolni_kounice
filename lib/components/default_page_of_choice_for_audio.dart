73182b589b25939cb90e74ee9cfc54f59830c5e0
Lukas Zobek <zoublu@email.cz>
2021-05-06 18:05:59 +0200

    vytvořen nový component default_page_of_choice_for_audio.dart , který bude obsahovat navíc FloatingActionButton pro zobrtazení modalBottomSheet s mapkou lokace

---
+++ lib/components/default_page_of_choice_for_audio.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:mesto_pamatek/constants/constants.dart';
+import 'package:mesto_pamatek/components/my_app_bar.dart';
+
+class DefaultPageOfChoice extends StatelessWidget {
+  DefaultPageOfChoice(
+      {@required this.tittleOfAppbar, @required this.listOfWidget});
+
+  final String tittleOfAppbar;
+  final List<Widget> listOfWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Město památek',
+      home: Scaffold(
+        backgroundColor: kBackgroundColor,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(kDefaultMyAppBarHeight),
+          child: MyAppBar(
+            tittleOfAppbar: tittleOfAppbar,
+            onPressedBackButton: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: listOfWidget,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
