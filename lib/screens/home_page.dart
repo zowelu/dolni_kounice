50f74939629a72654812eb9d88ec128465181e91
Lukas Zobek <zoublu@email.cz>
2021-05-07 19:15:49 +0200

    prevent back button přidán do home_page.dart

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -15,42 +15,45 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return DefaultPageOfChoice(
-      tittleOfAppbar: 'Město památek',
-      listOfWidget: [
-        Hero(tag: 'dk_logo',child: ContainerHeaderHomePage()),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Hero(
-              tag: 'audiopruvodce',
-              child: ChoiceContainer(
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: DefaultPageOfChoice(
+        tittleOfAppbar: 'Město památek',
+        listOfWidget: [
+          Hero(tag: 'dk_logo',child: ContainerHeaderHomePage()),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Hero(
+                tag: 'audiopruvodce',
+                child: ChoiceContainer(
+                    assetImageOfChoice:
+                        'assets/images/pamatky/klaster_rosa_coeli/klaster_krizova_chodba.jpg',
+                    textOfChoice: 'Audioprůvodce',
+                    onTap: () {
+                      Navigator.pushNamed(context, AudioGuide.id);
+                    }),
+              ),
+              Hero(
+                tag: 'pamatkyVeMeste',
+                child: ChoiceContainer(
+                    assetImageOfChoice:
+                        'assets/images/pamatky/mesto_pamatek_uvod/pamatky-mapa.jpg',
+                    textOfChoice: 'Památky ve městě',
+                    onTap: () {
+                      Navigator.pushNamed(context, Monuments.id);
+                    }),
+              ),
+              ChoiceContainer(
                   assetImageOfChoice:
-                      'assets/images/pamatky/klaster_rosa_coeli/klaster_krizova_chodba.jpg',
-                  textOfChoice: 'Audioprůvodce',
-                  onTap: () {
-                    Navigator.pushNamed(context, AudioGuide.id);
-                  }),
-            ),
-            Hero(
-              tag: 'pamatkyVeMeste',
-              child: ChoiceContainer(
-                  assetImageOfChoice:
-                      'assets/images/pamatky/mesto_pamatek_uvod/pamatky-mapa.jpg',
-                  textOfChoice: 'Památky ve městě',
-                  onTap: () {
-                    Navigator.pushNamed(context, Monuments.id);
-                  }),
-            ),
-            ChoiceContainer(
-                assetImageOfChoice:
-                    'assets/images/pamatky/mesto_pamatek_uvod/beautiful-scenery-greenfield-countryside-eifel-region-germany-M.jpg',
-                textOfChoice: 'Tipy na výlety v okolí',
-                onTap: () {}),
-          ],
-        ),
-      ],
+                      'assets/images/pamatky/mesto_pamatek_uvod/beautiful-scenery-greenfield-countryside-eifel-region-germany-M.jpg',
+                  textOfChoice: 'Tipy na výlety v okolí',
+                  onTap: () {}),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
