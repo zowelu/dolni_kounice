057b0c1bc4b57961a9c186f93620aba061818cb1
Lukas Zobek <zoublu@email.cz>
2021-05-10 17:35:47 +0200

    úprava vzhledu about_app_dialog.dart

--- lib/components/about_app_dialog.dart
+++ lib/components/about_app_dialog.dart
@@ -20,51 +20,88 @@ class _AboutAppDialogState extends State<AboutAppDialog> {
       child: contentBox(context),
     );
   }
-  contentBox(context){
+
+  contentBox(context) {
     return Stack(
       children: <Widget>[
         Container(
-          padding: EdgeInsets.only(left: kDefaultPadding,top: 30.0
-              + kDefaultPadding, right: kDefaultPadding,bottom: kDefaultPadding
-          ),
-          margin: EdgeInsets.only(top: 30.0),
+          padding: EdgeInsets.only(
+              left: kDefaultPadding,
+              top: 70.0 + kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: kDefaultPadding),
+          margin: EdgeInsets.only(top: kDefaultMarginLarger),
           decoration: BoxDecoration(
               shape: BoxShape.rectangle,
               color: Colors.white,
               borderRadius: BorderRadius.circular(kDefaultPadding),
               boxShadow: [
-                BoxShadow(color: Colors.black,offset: Offset(0,10),
-                    blurRadius: 10
-                ),
-              ]
-          ),
+                BoxShadow(
+                    color: Colors.black, offset: Offset(0, 10), blurRadius: 10),
+              ]),
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Text('Město památek',style: TextStyle(fontSize: 22,fontWeight: FontWeight.w600),),
-              SizedBox(height: 15,),
-              Text('Tato aplikace je poskytnuta zdarma Městu Dolní Kounice',style: TextStyle(fontSize: 14),textAlign: TextAlign.center,),
-              SizedBox(height: 22,),
+              Text(
+                'Město památek',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: kDefaultMarginLarger,
+              ),
+              Text(
+                'Tato aplikace je poskytnuta zdarma Městu Dolní Kounice.',
+                style: TextStyle(fontSize: 14),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: kDefaultMargin,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Aplikaci vytvořil',
+                    style: TextStyle(fontSize: 14),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    width: kDefaultMargin,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Image( height: 25,
+                      image: AssetImage('assets/images/webstrong-logo.png'),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 22,
+              ),
               Align(
                 alignment: Alignment.bottomRight,
                 child: TextButton(
-                    onPressed: (){
+                    onPressed: () {
                       Navigator.of(context).pop();
                     },
-                    child: Text('Zavřít',style: TextStyle(fontSize: 18),)),
+                    child: Text(
+                      'Zavřít',
+                      style: TextStyle(fontSize: 18),
+                    )),
               ),
             ],
           ),
         ),
         Positioned(
-          left: kDefaultPadding,
-          right: kDefaultPadding,
+          left: 100,
+          right: 100,
           child: CircleAvatar(
             backgroundColor: Colors.transparent,
-            radius: 30.0,
+            radius: 50.0,
             child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                child: Image.asset('assets/images/dk_znak_icon.png'),
+              borderRadius: BorderRadius.all(Radius.circular(50.0)),
+              child: Image.asset('assets/images/dk_znak_icon.png'),
             ),
           ),
         ),
