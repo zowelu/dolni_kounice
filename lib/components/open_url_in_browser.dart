dd4e2980a9a7092b77a1d5eb31669d48e67d3514
zoublu <zoublu@email.cz>
2021-05-13 04:54:46 +0200

    vytvořený soubor open_url_in_browser.dart

---
+++ lib/components/open_url_in_browser.dart
@@ -0,0 +1,23 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class OpenUrlInBrowser {
+  String _launchURL =
+      'https://www.dolnikounice.cz/klaster-rosa-coeli/d-78777/p1=4774';
+
+  Future<void> _launchInBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: false,
+        forceWebView: false,
+        headers: <String, String>{'header_key': 'header_value'},
+      );
+    } else {
+      throw 'Could not lauch $url';
+    }
+  }
+
+  Function openUrl() {
+    _launchInBrowser(_launchURL);
+  }
+}
