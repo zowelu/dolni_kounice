c194f70807734cce1a10944d587396a65f9936a6
zoublu <zoublu@email.cz>
2021-05-31 00:21:44 +0200

    instance třídy JevishSynagogue v image_gallery.dart

--- lib/components/image_gallery.dart
+++ lib/components/image_gallery.dart
@@ -1,22 +1,11 @@
+import 'package:dolni_kounice/components/jevish_synagogue.dart';
 import 'package:dolni_kounice/components/rosa_coeli.dart';
 import 'package:flutter/material.dart';
 
 class ImageGallery extends StatelessWidget {
   final List<String> imageGalleryRosaCoeli = RosaCoeli().imageGalleryRosaCoeli;
 
-  final List<String> imageGalleryJevishSynagogue = [
-    'assets/images/pamatky/synagoga/synagoga-venek.jpg',
-    'assets/images/pamatky/synagoga/synagoga-venek-3.jpg',
-    'assets/images/pamatky/synagoga/synagoga-venek-2.jpg',
-    'assets/images/pamatky/synagoga/synagoga-venek-5.jpg',
-    'assets/images/pamatky/synagoga/synagoga-venek-4.jpg',
-    'assets/images/pamatky/synagoga/synagoga-vnitrek-1.jpg',
-    'assets/images/pamatky/synagoga/synagoga-vnitrek-4.jpg',
-    'assets/images/pamatky/synagoga/synagoga-vnitrek-2.jpg',
-    'assets/images/pamatky/synagoga/synagoga-vnitrek-3.jpg',
-    'assets/images/pamatky/synagoga/synagoga-vnitrek-5.jpg',
-    'assets/images/pamatky/synagoga/synagoga-vnitrek-6.jpg',
-  ];
+  final List<String> imageGalleryJevishSynagogue = JevishSynagogue().imageGalleryJevishSynagogue;
 
   ///porovná zadaný text s názvem Listu galerie památky a vrátí ho
   // ignore: missing_return
