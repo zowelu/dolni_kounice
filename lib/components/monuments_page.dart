9c9c4ecb2cf4d8b9b6f8073499af44118e4862f4
Lukas Zoubek <zoublu@email.cz>
2021-05-30 17:27:51 +0200

    vyextrahování floatingButton z monuments_page.dart do floating_button.dart

--- lib/components/monuments_page.dart
+++ lib/components/monuments_page.dart
@@ -71,29 +71,6 @@ class MonumentsPage extends StatelessWidget {
   }
 }
 
-class FloatingButton extends StatelessWidget {
-  const FloatingButton({@required this.onPressedFloatingButton, @required this.textOfFloatingButton});
 
-  final String textOfFloatingButton;
-  final Function onPressedFloatingButton;
-
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton.extended(
-      label: Text(
-        textOfFloatingButton,
-        style: TextStyle(fontFamily: kDefaultFontFamily, fontSize: 20.0),
-      ),
-      elevation: 5,
-      backgroundColor: kBackgroundColor,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(30.0),
-          ),
-          side: BorderSide(width: 2.0, color: Colors.white)),
-      onPressed: onPressedFloatingButton,
-    );
-  }
-}
 
 
