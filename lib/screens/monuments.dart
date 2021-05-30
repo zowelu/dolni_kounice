592d84e4020edc7eb17f971cbdd6294f188662ab
Lukas Zoubek <zoublu@email.cz>
2021-05-30 17:09:09 +0200

    Implementace parametru onPressedFloatingButton v monuments.dart a přejmenování Hero tag a titleOfAppBar

--- lib/screens/monuments.dart
+++ lib/screens/monuments.dart
@@ -1,7 +1,9 @@
+import 'package:dolni_kounice/components/default_page_of_choice_with_floating_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:dolni_kounice/constants/constants.dart';
 import 'package:dolni_kounice/components/choice_container.dart';
 import 'package:dolni_kounice/components/container_header_image_background.dart';
-import 'package:dolni_kounice/components/default_page_of_choice.dart';
 import 'package:dolni_kounice/screens/monuments_jevish_synagogue.dart';
 import 'package:dolni_kounice/screens/monuments_rosa_coeli.dart';
 
@@ -14,15 +16,64 @@ class Monuments extends StatefulWidget {
 class _MonumentsState extends State<Monuments> {
   @override
   Widget build(BuildContext context) {
-    return DefaultPageOfChoice(
-      tittleOfAppbar: 'Památky ve městě',
+    return DefaultPageOfChoiceWithFloatingButton(
+      tittleOfAppbar: 'Průvodce památkami',
+      textOfFloatingButton: 'Mapa památek',
+      assetImageOfModal: 'assets/images/DK_znak_200px.png',
+      onPressedFloatingButton: (){
+        showModalBottomSheet(
+          context: context,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
+            ),
+          ),
+          isScrollControlled: false,
+          clipBehavior: Clip.antiAlias,
+          backgroundColor: kBackgroundColor,
+          builder: (context) => Container(
+            //height: MediaQuery.of(context).size.height/3*2.5,
+            child: Container(
+              padding: EdgeInsets.all(kDefaultPadding),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FractionallySizedBox(
+                    widthFactor: 0.15,
+                    child: Container(
+                      height: 5.0,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(2.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: kDefaultMarginLarger),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: Image(
+                        image: AssetImage('assets/images/pamatky/mesto_pamatek_uvod/pamatky-mapa.jpg'),
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+        );
+      },
       listOfWidget: [
         Hero(
-            tag: 'pamatkyVeMeste',
+            tag: 'pruvodcePamatkami',
             child: ContainerHeaderImageBackground(
               assetImage:
                   'assets/images/pamatky/mesto_pamatek_uvod/pamatky-mapa.jpg',
-              textHeader: 'Památky ve městě',
+              textHeader: 'Průvodce památkami',
               text: '',
             )),
         Hero(
