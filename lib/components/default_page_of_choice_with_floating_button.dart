9e8e2f6c0c02130640fdb6f852f43da8f1c290aa
Lukas Zoubek <zoublu@email.cz>
2021-05-30 17:44:54 +0200

    implementace FloatingButton do default_page_of_choice_with_floating_button.dart

--- lib/components/default_page_of_choice_with_floating_button.dart
+++ lib/components/default_page_of_choice_with_floating_button.dart
@@ -1,3 +1,4 @@
+import 'package:dolni_kounice/components/floating_button.dart';
 import 'package:flutter/material.dart';
 import 'package:dolni_kounice/constants/constants.dart';
 import 'package:dolni_kounice/components/my_app_bar.dart';
@@ -47,20 +48,8 @@ class DefaultPageOfChoiceWithFloatingButton extends StatelessWidget {
             ),
           ),
         ),
-        floatingActionButton: FloatingActionButton.extended(
-          label: Text(
-            textOfFloatingButton,
-            style: TextStyle(fontFamily: kDefaultFontFamily, fontSize: 20.0),
-          ),
-          elevation: 5,
-          backgroundColor: kBackgroundColor,
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(30.0),
-              ),
-              side: BorderSide(width: 2.0, color: Colors.white)),
-          onPressed: onPressedFloatingButton,
-        ),
+        floatingActionButton: FloatingButton(onPressedFloatingButton: onPressedFloatingButton, textOfFloatingButton: textOfFloatingButton),
+
       ),
     );
   }
