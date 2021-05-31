8feb4f52808f2b679ce372bcb1b974243ff64492
zoublu <zoublu@email.cz>
2021-05-31 23:59:17 +0200

    vytvoření choiceContainer pro Antoníček

--- lib/screens/monuments.dart
+++ lib/screens/monuments.dart
@@ -1,7 +1,9 @@
 import 'package:dolni_kounice/components/monuments/castle_and_chateau.dart';
+import 'package:dolni_kounice/components/monuments/chapel_of_st_antonine.dart';
 import 'package:dolni_kounice/components/monuments/jevish_synagogue.dart';
 import 'package:dolni_kounice/components/default_page_of_choice_with_floating_button.dart';
 import 'package:dolni_kounice/components/monuments/rosa_coeli.dart';
+import 'package:dolni_kounice/screens/monuments/monuments_chapel_of_st_antonine.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:dolni_kounice/constants/constants.dart';
@@ -110,6 +112,17 @@ class _MonumentsState extends State<Monuments> {
             textOfChoice: CastleAndChateau().name,
             onTap: () {
               Navigator.pushNamed(context, MonumentsCastleAndChateau.id);
+            },
+          ),
+        ),
+        Hero(
+          tag: ChapelOfStAntonine().tag,
+          child: ChoiceContainer(
+            assetImageOfChoice:
+            ChapelOfStAntonine().imageGallery[0],
+            textOfChoice: ChapelOfStAntonine().name,
+            onTap: () {
+              Navigator.pushNamed(context, MonumentsChapelOfStAntonine.id);
             },
           ),
         ),
