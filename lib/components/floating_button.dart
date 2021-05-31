d167c87437258a67570b53a18ca77545fed95b68
zoublu <zoublu@email.cz>
2021-05-31 04:06:34 +0200

    floatingButton odstranění rámečku

--- lib/components/floating_button.dart
+++ lib/components/floating_button.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:dolni_kounice/constants/constants.dart';
 
 class FloatingButton extends StatelessWidget {
-  const FloatingButton({@required this.onPressedFloatingButton, @required this.textOfFloatingButton});
+  const FloatingButton(
+      {@required this.onPressedFloatingButton,
+      @required this.textOfFloatingButton});
 
   final String textOfFloatingButton;
   final Function onPressedFloatingButton;
@@ -17,10 +19,10 @@ class FloatingButton extends StatelessWidget {
       elevation: 5,
       backgroundColor: kBackgroundColor,
       shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(30.0),
-          ),
-          side: BorderSide(width: 2.0, color: Colors.white)),
+        borderRadius: BorderRadius.all(
+          Radius.circular(30.0),
+        ),
+      ),
       onPressed: onPressedFloatingButton,
     );
   }
