91d1cfc767e7984da90ac2ee726bb6b79c35c55c
zoublu <zoublu@email.cz>
2021-05-31 23:34:37 +0200

    vytvoření souboru pro antoníček a kopie základní struktury informací

---
+++ lib/components/monuments/chapel_of_st_antonine.dart
@@ -0,0 +1,84 @@
+class ChapelOfStAntonine {
+  String id = '/monumentsRosaCoeli';
+  String name = 'Klášter Rosa Coeli';
+  String url = 'https://www.dolnikounice.cz/klaster-rosa-coeli/d-78777/p1=4774';
+  String tag = 'monumentsRosaCoeli';
+  String nameOfImageGallery = 'imageGalleryRosaCoeli';
+
+  ///List názvů kapitol
+  List<String> chapter = [
+    'Úvod',
+    '1 - Portál',
+    '2 - Klášterní kostel',
+    '3 - Věžička',
+    '4 - Příčná chrámová loď',
+    '5 - Rajská zahrada',
+    'Historie',
+    'FilmyASeriály',
+    'Střecha',
+  ];
+
+  List<String> mp3 = [
+    'audio/rosa_coeli/uvod.mp3',
+    'audio/rosa_coeli/1.mp3',
+    'audio/rosa_coeli/2.mp3',
+    'audio/rosa_coeli/3.mp3',
+    'audio/rosa_coeli/4.mp3',
+    'audio/rosa_coeli/5.mp3',
+    'audio/rosa_coeli/historie.mp3',
+    'audio/rosa_coeli/filmy.mp3',
+    'audio/rosa_coeli/strecha.mp3',
+  ];
+
+  ///Map jednotlivých kapitol audioprůvodce a jejich přepisu
+  Map<String, String> audioText = {
+    'Úvod':  'Vítáme Vás v Dolních Kounicích a Klášteře Rosa coeli.\n\nKlášter se řadí k nejvýznamnějším středoevropským církevním stavbám vrcholné gotiky. Je nejstarší ženský premonstrátský klášter na Moravě i nejstarší šlechtickou fundací. Spolu s hradem tvoří harmonický celek, nemající v našich zemích obdoby.\n\nKlášter Rosa coeli (Růže nebeská) byl založen roku 1181 Vilémem z Pulína jako podmínka klatby, která na něho byla uvalena papežem za to, že plenil a pustošil rakouské kláštery a kostely. Musel učinit pokání a postavit klášter.\n\nŽenský premonstrátský klášter byl zasvěcený Panně Marii. První sestry premonstrátského řádu sem přišly o dva roky později v roce 1183 i se svým proboštem Petrem z Louňovic v Jižních Čechách.',
+    '1 - Portál': 'Svým pojetím i sochařským provedením patří vstupní portál k ojedinělým dílům tohoto typu u nás.\n\n Dominantní je na něm reliéf Krista, který pravicí žehná a v levé ruce drží knihu života, u hlavy má písmena alfa a omega, což znamenalo, že Kristus je počátkem a koncem všeho. Pod jeho rukama je hlava muže a ženy.\n\n Portál je zakončen trojúhelníkovým štítem (vimperkem), do jehož plochy je vkomponovaná panelová kružba trojlistu. Vimperk zakončuje kamenná kytice.',
+    '2 - Klášterní kostel': 'Klášterní kostel má půdorys tvaru kříže. Hlavní chrámová loď měří 45 m na délku a 10 m na šířku. V přední části klášterního kostela v tzv. presbytáři se nacházel hlavní oltář zasvěcený Panně Marii.\n\n V západní části hlavní lodě byl chór. Přístup na chór byl z prvního patra jižního křídla křížové chodby nebo portálem ze schodiště věžice. Kvalitní sochařské zpracování spolu s ostatními vyspělými kamenickými detaily jednoznačně ukazují, že v Kounicích pracovala stavební huť nejen důvěrně obeznámená s tvaroslovím obvyklým v parléřovské huti Karla IV., ale i dosahující jejích kvalit.',
+    '3 - Věžička': 'Ve vstupní části chrámu se po pravé straně nachází malý gotický portálek vedoucí na vřetenové schodiště ve věžici, která vystupuje z jižní zdi lodi a je završena zděnou jehlancovou střechou. Po úzkých schodech je možné vystoupit až nad koruny klášterního zdiva, odkud se otvírá nádherný výhled na klášterní areál a pohled na nedaleký hrad a zámek.',
+    '4 - Příčná chrámová loď': 'Příčná chrámová loď - transept – na jižní straně byl údajně oltář zasvěcený sv. Fabiánovi a Šebestiánovi, na levé straně pak oltář zasvěcený sv. Janu Křtiteli. V křížení příčné a hlavní lodi je údajně velmi silné energetické místo. Portálem v jižní zdi kněžiště se vstupuje do sakristie. Sakristii tvoří prostorná síň s křížovou klenbou sbíhající se do středního svazkového pilíře. Klenba byla rekonstruována z dochovaných architektonických článků a doplňků v letech 1926-1949 spolkem „Komite pro záchranu památek v Dolních Kounicích.“',
+    '5 - Rajská zahrada': 'Ke klášternímu kostelu přiléhá na severní straně lodi křížová chodba s rajským dvorem.\n\nPřízemí křížové chodby je zaklenuto 20 poli křížové klenby. Svorník jihozápadního pole nese nápis RESTAVRATVM A. MDCCCI V. A. S. – „Obnoveno roku 1701 Vít opat Strahovský“. Při archeologickém průzkumu byly pod její podlahou objeveny hroby ve třech vrstvách nad sebou. Do prvního patra křížové chodby se vstupuje po dodatečně přistavěném zalamovaném schodišti. Tradují se pověsti o zazděné jeptišce a o zlaté kachně.\n\nV klášteře, především v přízemí křížové chodby, jsou shromážděny některé architektonické články a jiné součásti jeho někdejší výbavy nalezené při zabezpečovacích pracích zříceniny. Některé fragmenty pocházejí z jiných míst v Dolních Kounicích, zejména z hradu a zámku.\n\nMezi nejvýznamnější patří náhrobek brněnského a olomouckého kanovníka Dětřicha z Jindřichova Hradce, jenž zemřel 28. října 1349 s rytou kresbou kněze držícího kalich, torzo pozdně gotického figurálního náhrobku dolnokounického probošta Jiřího z doby okolo roku 1500 a fragment mramorového renesančního náhrobku s hlavou šlechtičny a znakem Žabků z Limberka (snad Anny Žabkové z doby po r. 1588). Další památky z dolnokounického kláštera jsou umístěny v Moravském zemském muzeu a v Moravské galerii v Brně. První patro bylo do rajského dvora otevřeno pravoúhlými gotickými okny s dělícím středním kamenným křížem. Z původních kleneb se dodnes dochovaly pouze baldachýnové konzoly.\n\nV přízemí východního křídla křížové chodby se nachází vstup do kapitulní síně zaklenuté dvěma poli žebrové křížové klenby. Žebra hruškového profilu se skosenou hranou se kříží v kruhových svornících s reliéfy pelikána (na východě) a beránka. Pod individuálně rozčleněnou krycí deskou konzol jsou čtyři lidské masky s věncem dubových listů a hlavy berana a lva. Kapitulní síň je vůbec nejcelistvěji dochovaným prostorem celého areálu.',
+    'Historie': 'Od Viléma z Pulína dostal klášter v užívání část zakladatelova majetku. Zbožnými odkazy se klášterní záduší zvětšovalo, klášter získal více členů (členek) a byl povýšen na proboštství. V roce 1268 vzal dokonce klášter pod svůj patronát papež Kliment IV. a klášter měl právo vybírat desátky ze 16 kostelů na Moravě. Král Václav II. dne 8. dubna 1284 oznamuje, že je kounický klášter klášterem královským. Je tedy pod ochranou samotného panovníka. Kolem roku 1328 nastává období velkého rozkvětu kláštera a kvůli obraně začíná stavba hradu.\n\nKlášter zažil na přelomu 13. a 14. století velký hospodářský rozkvět. Vlastnil rozsáhlé majetky, dostával spoustu darů a dobře hospodařil. Roku 1423 byl klášter vypálen husitskými vojsky. Po požáru byla nad chrámem obnažena klenba, která se z povětrnostních vlivů zřítila. Chrámová loď byla poté zakryta pouze trámovým stropem a původní klenba již nebyla nikdy obnovena. Na počátku 16. století, po nástupu nového probošta Martina Göschla, nastal velký úpadek kláštera. Göschl měl hodnost světícího biskupa ale přestoupil na luteránské vyznání a oženil se s jednou místní řádovou sestrou. Roku 1526 utekl do Mikulova a dal se k novokřtěncům. Poté byl mučen a zemřel v olomouckém vězení. Zbylých 11 řeholnic si zvolilo nového probošta, kněze Jana. Dle pověsti, jejich neřestný život pobouřil místní obyvatelstvo, které klášter přepadlo zneuctilo a vypálilo.\n\nOpuštěného kláštera se roku 1527 ujaly Moravské zemské stavy. Ty předaly klášter českému králi Ferdinandu I. Habsburskému. Ten klášter nejprve zastavil a na konec prodal roku 1537 Jiřímu Žabkovi z Limberka. Jiří Žabka si Dolní Kounice zvolil za své hlavní sídlo. Přebudoval klášterní hrad a opravil klášterní chrám, ve kterém si zřídil rodinnou hrobku. Jeho syn Burian celé jmění a majetek prohospodařil. Za ostatních majitelů panství klášter pustl. O obnovu opuštěného kláštera se pokusili premonstráti ze strahovského kláštera. Roku 1698 budovu odkoupili od tehdejšího majitele panství Ferdinanda z Ditrichštejna. V následujícím roce upravili bývalou kapitulní síň na kapli a směrem k řece nechali zrekonstruovat barokní budovu konventu. V přízemí křížové chodby byla dvě zřícená klenební pole obnovená z cihel tak, aby napodobovala původní architekturu. Jde tedy o jeden z nejstarších projevů památkové péče u nás.\n\nRoku 1703 byl nově zastřešený kostel znovu vysvěcen. Toho roku však Dolní Kounice zachvátil obrovský požár, který zničil i zrestaurovaný klášter. Poté byla opravena již jen budova konventu. Kostel byl ponechán ve zříceninách a obřady se konaly pouze v kapli. Špatné ekonomické podmínky nedovolovaly rozsáhlejší opravy kláštera, a tak byly i mše v kapli zakázány. Strahovští premonstráti roku 1808 prodali klášter s kostelem zpět dolnokounickému panství. Od té doby se budovy začaly definitivně měnit ve zříceniny. V roce 1808 byl klášter odsvěcen.\n\nV současné době je majitelem kláštera Biskupství brněnské. Pronajímatelem je Město Dolní Kounice které zabezpečuje průvodcovskou činnost a pořádá kulturní akce. Každým rokem jsou na opravu a sanaci kláštera věnovány velké finanční prostředky.',
+    'Které filmy a seriály se v klášteře natáčely?': 'Jaké filmy a seriály se v klášteře natáčeli?\n\n-	Záhada Freyerského opatství\n-	O statečném kováři\n-	Lekce Faust\n-	Četnické humoresky\n-	Záhady Toma Wizarda\n-	Hlas pro římského krále\n-	Labyrint II.\n-	Dobré ráno\n-	Pohádky krále Jiřího – čtení do ouška\na Toulavá kamera',
+    'Proč klášter nemá střechu?': 'Proč klášter nemá střechu?\n\n Protože jej roku 1423 vyplenila a zapálila husitská vojska. Chrámová loď poté byla zakryta pouze trámovým stropem a původní klenba již nikdy nebyla obnovena. Roku 1703 Dolní Kounice zachvátil obrovský požár, který zničil i zrestaurovaný klášter. Poté byla opravena jen budova konventu, kostel byl ponechán ve zříceninách.'
+  };
+
+  ///List obrázků pro použití nejen v galerii
+  List<String> imageGallery = [
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-pohled-brana.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-pohled-zepredu.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-portal.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-chram-1.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-chram-2.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-chram-slunce.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster_krizova_chodba.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klasterni-zahrada.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-rajska-zahrad.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-zad.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-kamenny-nahrobek.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-1200.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-1400.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-1423.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-1720.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-letecky.jpg',
+  ];
+
+  ///List zbytků obrázků
+  List<String> images = [
+    'assets/images/pamatky/klaster_rosa_coeli/mapa_klaster.png',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster-portal-vstupni.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/klaster_rosa_coeli_filmy_serialy.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/photographic-film-wooden-background_compressed.jpg',
+    'assets/images/pamatky/klaster_rosa_coeli/rosa_coeli_pudorys.png',
+  ];
+
+  ///vezme text přepisu audioprůvodce na základě klíče
+  String getAudioTextOfMap(String keyOfMap){
+
+    return audioText[keyOfMap];
+  }
+  ///zjistí počet textů přepisů
+  int getMapLength(){
+    return audioText.length;
+  }
+}
