2d2cd6f3a993a06f728dcef1733adcba58a489e2
zoublu <zoublu@email.cz>
2021-06-01 01:53:48 +0200

    kompletování monuments barborky v monuments_st_barbora_church.dart

--- lib/screens/monuments/monuments_st_barbora_church.dart
+++ lib/screens/monuments/monuments_st_barbora_church.dart
@@ -31,10 +31,10 @@ class MonumentsStBarbaraChurch extends StatelessWidget {
       listOfWidget: [
         TextDefaultStandart(
             text:
-            'Poutní kaple sv. Antonína se vypíná na stejnojmenném kopci nad řekou a je působivým objektem poutajícím ze široké vzdálenosti pozornost návštěvníků města.\n\nKaple je zavřená a přístupná pouze během pouti v červnu.'),
+            'Zčásti skrytá lidskému zraku, přesto po několik století součást dolnokounické historie. To je malebná, nedávno zrekonstruovaná kaple, lidově zvaná Barborka.'),
         SizedBox(height: kDMarginLarger),
         ChoiceContainer(
-            assetImageOfChoice: stBarbaraChurch.imageGallery[1],
+            assetImageOfChoice: stBarbaraChurch.imageGallery[4],
             textOfChoice: 'Galerie',
             onTap: () {
               Navigator.push(
@@ -66,41 +66,8 @@ class MonumentsStBarbaraChurch extends StatelessWidget {
         SizedBox(height: kDMargin),
         TextDefaultStandart(
           text:
-          'Kaple sv. Antonína jako dřevěná byla postavena v r. 1654. O sto let později pak byla nahrazena stavbou zděnou, neboť nestačila velkému počtu poutníků. Náklady na stavbu hradil kníže Karel Maxmilián z Dietrichsteina. Kaple byla postavena brněnským stavitelem Františkem Antonínem Grimmem, který žil v letech 1710 až 1784. Tento barokní stavitel a architekt projektoval např. Zábrdovický zámek a zámek v Napajedlech. Je pohřben i se svým otcem v brněnské kapucínské hrobce. Podle jeho projektu byl také stavěn zámek v Kupařovicích. Byl dvorním stavitelem knížat z Dietrichsteina v té době. Budova kaple má velkou klenutou kopuli a tři vchody - hlavní a dva boční.\n\nJak uvádí zakládací listina z 30. dubna 1654, její vybudování iniciovali dolnokouničtí občané k poctě patronů sv. Antonína Padovánského a sv. Floriána… „aby se přimluvili za obyvatele, kteří každoročně strádají různými ohni a záplavami“. Ke stavbě přispěly okolní obce Pravlov, Němčičky, Kupařovice, Malešovice, Odrovice a další z kounického knížecího panství.',
+          'Kaple v dnešní podobě byla vystavěna Jiřím Richtrem z Brna za Ferdinanda Ditrichsteina v r. 1688. Gotické kněžiště z původního sboru v upravené podobě zůstalo zachováno a k němu byla přistavěna renesanční přístavba vlastní kaple, nad jejímž vchodem je znak Dietrichsteinů (vinařské nože) s uvedeným letopočtem. Původně zde měla sekta bratří Valdenských a Boleslavských svoji modlitebnu - sbor v gotickém slohu - které Jan ze Žerotína daroval svůj dům. V roce 1560 k němu daroval dům také Jiří ze Zástřizl, aby v něm správce sboru bydlel a bratří měli kde provozovat své řemeslo.\n\nPo zatopení a uzavření starého farního kostela u řeky v r. 1862 zde byly se souhlasem mikulovských Dietrichsteinů až do roku 1879 konány všechny katolické bohoslužby, křty, svatby a pohřby. Kaple ovšem tomuto účelu sloužila jen jako nevyhovující, malé provizorium a to jen do doby, než byl postaven a otevřen nový farní kostel.\n\nPo první světové válce v r. 1921 kapli sv. Fabiána a Šebestiána kounické obecní zastupitelstvo pronajalo nově vzniklé Církvi československé husitské na dobu 15 let. V devadesátých letech 20. století byla kaple za pomoci města důkladně opravena a pravidelně se zde konají nedělní pravoslavné bohoslužby. Bývají zde také pořádány výstavy nebo koncerty duchovní a vážné hudby, jako např. každoročně ve vánočním období nebo i při jiných příležitostech v průběhu roku. Kaple je majetkem Pravoslavné církevní obce.',
         ),
-        SizedBox(
-          height: kDMargin,
-        ),
-        ImageOnPageWithOnTap(
-          assetImage: stBarbaraChurch.imageGallery[3],
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => SinglePhotoViewPage(
-                    assetImage: stBarbaraChurch.imageGallery[3]),
-              ),
-            );
-          },
-        ),
-        ImageOnPageWithOnTap(
-          assetImage: stBarbaraChurch.imageGallery[4],
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => SinglePhotoViewPage(
-                    assetImage: stBarbaraChurch.imageGallery[4]),
-              ),
-            );
-          },
-        ),
-        SizedBox(
-          height: kDMargin,
-        ),
-        TextDefaultStandart(
-            text:
-            'Kapličky - jednotlivá zastavení křížové cesty - byly vybudovány někdy na počátku 19. století. Roku 1899 byly do jednotlivých zastavení pořízeny z Blanska litinové plastiky, každá o hmotnosti padesát kilogramů. Původně tam byly obrazy staré křížové cesty z kostela u řeky, jenž byl po povodni roku 1862 zbourán. Staré obrazy ze zastavení se poděly neznámo kam.\n\nV roce 1948 byla kaple, poškozená válečnými událostmi, opravena. Další generální oprava začala roku 2013. Práce začaly vysekáním dřevin a od července probíhaly hlavní práce na střeše. Rekonstrukce kaple pokračovala v roce 2015 opravou fasády.\n\nTradiční pouť ke svatému Antonínovi se koná každoročně v červnu. Majitelem kaple je církev Římskokatolická.'),
       ],
     );
   }
