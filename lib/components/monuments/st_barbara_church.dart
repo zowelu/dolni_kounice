6e768683f4db1082281d79142466a0356e734e4f
zoublu <zoublu@email.cz>
2021-06-01 00:43:29 +0200

    úprava List imageGallery v st_barbara_church.dart

--- lib/components/monuments/st_barbara_church.dart
+++ lib/components/monuments/st_barbara_church.dart
@@ -23,31 +23,16 @@ class StBarbaraChurch{
 
   ///List obrázků pro použití nejen v galerii
   List<String> imageGallery = [
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-pohled-brana.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-pohled-zepredu.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-portal.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-chram-1.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-chram-2.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-chram-slunce.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster_krizova_chodba.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klasterni-zahrada.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-rajska-zahrad.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-zad.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-kamenny-nahrobek.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-1200.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-1400.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-1423.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-1720.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-letecky.jpg',
+    'assets/images/pamatky/kaple_sv_barbory/kaple-sv-barbory-venek-1.jpg',
+    'assets/images/pamatky/kaple_sv_barbory/kaple-sv-barbory-venek-2.jpg',
+    'assets/images/pamatky/kaple_sv_barbory/kaple-sv-barbory-venek-3.jpg',
+    'assets/images/pamatky/kaple_sv_barbory/kaple-sv-barbory-vnitrek-1.jpg',
+    'assets/images/pamatky/kaple_sv_barbory/kaple-sv-barbory-vnitrek-2.jpg',
+    'assets/images/pamatky/kaple_sv_barbory/kaple-sv-barbory-vnitrek-3.jpg',
   ];
 
   ///List zbytků obrázků
   List<String> images = [
-    'assets/images/pamatky/klaster_rosa_coeli/mapa_klaster.png',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster-portal-vstupni.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/klaster_rosa_coeli_filmy_serialy.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/photographic-film-wooden-background_compressed.jpg',
-    'assets/images/pamatky/klaster_rosa_coeli/rosa_coeli_pudorys.png',
   ];
 
   ///vezme text přepisu audioprůvodce na základě klíče
