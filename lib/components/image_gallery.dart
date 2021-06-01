f23857ffa3739bb37e4654e3161b2450d8472294
zoublu <zoublu@email.cz>
2021-06-01 02:28:56 +0200

    vytvořena galerie pro kostel v image_gallery.dart

--- lib/components/image_gallery.dart
+++ lib/components/image_gallery.dart
@@ -1,5 +1,6 @@
 import 'package:dolni_kounice/components/monuments/castle_and_chateau.dart';
 import 'package:dolni_kounice/components/monuments/chapel_of_st_antonine.dart';
+import 'package:dolni_kounice/components/monuments/church_of_st_peter_and_pavel.dart';
 import 'package:dolni_kounice/components/monuments/jevish_synagogue.dart';
 import 'package:dolni_kounice/components/monuments/rosa_coeli.dart';
 import 'package:dolni_kounice/components/monuments/st_barbara_church.dart';
@@ -19,6 +20,9 @@ class ImageGallery {
   final List<String> imageGalleryStBarbaraChurch =
       StBarbaraChurch().imageGallery;
 
+  final List<String> imageGalleryChurchOfStPeterAndPavel =
+      ChurchOfStPeterAndPavel().imageGallery;
+
   ///porovná zadaný text s názvem Listu galerie památky a vrátí ho
   // ignore: missing_return
   List getImageGallery(String nameOfImageGallery) {
@@ -32,8 +36,8 @@ class ImageGallery {
       return imageGalleryChapelOfStAntonine;
     } else if (nameOfImageGallery == 'imageGalleryStBarbaraChurch') {
       return imageGalleryStBarbaraChurch;
-    } else if (nameOfImageGallery == 'imageGalleryJevishSynagogue') {
-      return imageGalleryJevishSynagogue;
+    } else if (nameOfImageGallery == 'imageGalleryChurchOfStPeterAndPavel') {
+      return imageGalleryChurchOfStPeterAndPavel;
     }
   }
 }
