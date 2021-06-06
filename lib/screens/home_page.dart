04d749bcf53650b6b0f073b5fac2ba190a40e4e6
Lukas Zoubek <zoublu@email.cz>
2021-06-06 17:55:30 +0200

    do choiceContainer pro Turistické trasy a služby přidány url odkazy na městské stránky

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:dolni_kounice/screens/map_of_sights.dart';
+import 'package:dolni_kounice/components/open_url_in_browser.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:dolni_kounice/components/default_page_of_choice.dart';
@@ -68,18 +68,24 @@ class _HomePageState extends State<HomePage> {
                     assetImageOfChoice:
                         'assets/images/pamatky/mesto_pamatek_uvod/pexels-elina-sazonova-2705756_compressed.jpg',
                     textOfChoice: 'Turistické trasy',
-                    onTap: () {}),
+                    onTap: () async {
+                      //po stisknutí otevře stránku v externím prohlížeči
+                      OpenUrlInBrowser().openUrl(
+                          'https://www.dolnikounice.cz/pesi-turistika/ds-4473/archiv=0&p1=4752');
+                    },),
               ),
-              ChoiceContainer(
-                  assetImageOfChoice:
-                      'assets/images/pamatky/mesto_pamatek_uvod/pexels-chevanon-photography-302899_compressed.jpg',
-                  textOfChoice: 'Služby',
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => MapOfSights()),
-                    );
-                  }),
+              Hero(
+                tag: 'sluzby',
+                child: ChoiceContainer(
+                    assetImageOfChoice:
+                        'assets/images/pamatky/mesto_pamatek_uvod/pexels-chevanon-photography-302899_compressed.jpg',
+                    textOfChoice: 'Služby',
+                    onTap: () async {
+                      //po stisknutí otevře stránku v externím prohlížeči
+                      OpenUrlInBrowser().openUrl(
+                          'https://www.dolnikounice.cz/sluzby-ve-meste-a-okoli/os-1121/p1=8934');
+                    },),
+              ),
             ],
           ),
         ],
