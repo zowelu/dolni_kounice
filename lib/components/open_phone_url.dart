eaa8a92c72fcfa030b45dff7d8a46d6b36db8eef
Lukas Zoubek <zoublu@email.cz>
2021-07-07 21:32:09 +0200

    vytvořen soubor s funkcí otevření dialeru

---
+++ lib/components/open_phone_url.dart
@@ -0,0 +1,19 @@
+import 'package:url_launcher/url_launcher.dart';
+
+//funkce otevírající stránku v externím prohlížeči
+class OpenPhoneURL {
+  //String _launchURL ='https://www.dolnikounice.cz/klaster-rosa-coeli/d-78777/p1=4774';
+
+  _launchPhoneURL(String phoneNumber) async {
+    String url = 'tel:' + phoneNumber;
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  void makePhoneCall(String launchURL) {
+    _launchPhoneURL(launchURL);
+  }
+}
