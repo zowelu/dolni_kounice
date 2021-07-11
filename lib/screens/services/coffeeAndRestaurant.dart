05c8d0e9b796c6abb9c2e2c3a0c6043604d8192e
zoublu <zoublu@email.cz>
2021-07-12 01:55:22 +0200

    vytvořené komplet coffeeAndRestaurant.dart

--- lib/screens/services/coffeeAndRestaurant.dart
+++ lib/screens/services/coffeeAndRestaurant.dart
@@ -1,18 +1,7 @@
-import 'package:dolni_kounice/components/image_on_page_with_ontap.dart';
-import 'package:dolni_kounice/components/monuments/castle_and_chateau.dart';
-import 'package:dolni_kounice/components/monuments/rosa_coeli.dart';
-import 'package:dolni_kounice/components/text_default_standart.dart';
-import 'package:dolni_kounice/components/text_default_standart_bold.dart';
-import 'package:dolni_kounice/screens/monuments/monuments_rosa_coeli.dart';
 import 'package:flutter/material.dart';
-import 'package:dolni_kounice/constants/constants.dart';
 import 'package:dolni_kounice/components/monuments_page.dart';
-import 'package:dolni_kounice/components/open_url_in_browser.dart';
-import 'package:dolni_kounice/components/my_button.dart';
-import 'package:dolni_kounice/components/monuments/jevish_synagogue.dart';
-import 'package:dolni_kounice/components/single_photo_view_page.dart';
-import 'package:dolni_kounice/screens/monuments/monuments_jevish_synagogue.dart';
 import 'package:dolni_kounice/components/card_of_subject.dart';
+import 'package:dolni_kounice/components/divider_text.dart';
 
 class CoffeeAndRestaurant extends StatelessWidget {
   static String id = '/cofeeAndRestaurant';
@@ -43,13 +32,7 @@ class CoffeeAndRestaurant extends StatelessWidget {
           label:
               'Platba kartou, Se psem, Bezbariérové, Dětský koutek, Zahrádka',
         ),
-        SizedBox(height: kDMarginLarger),
-        Divider(
-          height: 5,
-          color: kDividerColor,
-          thickness: 5,
-        ),
-        SizedBox(height: kDMarginLarger),
+        DividerText(),
         CardOfSubject(
             name: 'Hotel Vinum coeli',
             phoneNumber: '730 825 882',
@@ -59,179 +42,47 @@ class CoffeeAndRestaurant extends StatelessWidget {
             addressURL:
                 'http://www.google.com/maps?q=49.0700406,16.464826&z=14&t=m',
             label: 'Platba kartou, Denní menu, Zahrádka'),
-        SizedBox(height: kDMarginLarger),
-        Divider(
-          height: 5,
-          color: kDividerColor,
-          thickness: 5,
-        ),
-        SizedBox(height: kDMarginLarger),
-        TextDefaultStandartBold(text: '1. Židovská synagoga \(cca 30 minut\)'),
-        SizedBox(height: kDMargin),
-        ImageOnPageWithOnTap(
-          assetImage: JevishSynagogue().imageGallery[7],
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => SinglePhotoViewPage(
-                    assetImage: JevishSynagogue().imageGallery[7]),
-              ),
-            );
-          },
-        ),
-        SizedBox(
-          height: kDMarginLarger,
-        ),
-        TextDefaultStandart(
-            text:
-                'Možnost samostatné prohlídky s tištěným průvodcem nebo s komentářem průvodce, dle počtu návštěvníků a vytíženosti průvodce.\n\nMožnost soukromé prohlídky mimo otevírací dobu.'),
-        SizedBox(
-          height: kDMarginLarger,
-        ),
-        Center(
-          child: Container(
-            width: double.infinity,
-            child: MyButton(
-              textOfButton: 'Přejít na stránku památky',
-              onPressed: () {
-                Navigator.pushNamed(context, MonumentsJevishSynagogue.id);
-              },
-            ),
-          ),
-        ),
-        SizedBox(
-          height: kDMargin,
-        ),
-        Center(
-          child: Container(
-            width: double.infinity,
-            child: MyButton(
-              textOfButton:
-                  'Informace o vstupném, otevírací době a dalších naleznete zde',
-              onPressed: () async {
-                //po stisknutí otevře stránku v externím prohlížeči
-                OpenUrlInBrowser().openUrl(JevishSynagogue().url);
-              },
-            ),
-          ),
-        ),
-        SizedBox(height: kDMarginLarger),
-        Divider(
-          height: 5,
-          color: kDividerColor,
-          thickness: 5,
-        ),
-        SizedBox(height: kDMarginLarger),
-        TextDefaultStandartBold(text: '2. Klášter Rosa coeli \(cca 45 minut\)'),
-        SizedBox(height: kDMargin),
-        ImageOnPageWithOnTap(
-          assetImage: RosaCoeli().imageGallery[15],
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => SinglePhotoViewPage(
-                    assetImage: RosaCoeli().imageGallery[15]),
-              ),
-            );
-          },
-        ),
-        SizedBox(
-          height: kDMarginLarger,
-        ),
-        TextDefaultStandart(
-            text:
-                'Možnost samostatné prohlídky s tištěným průvodcem nebo s komentářem průvodce, dle počtu návštěvníků a vytíženosti průvodce.\n\nMožnost soukromé prohlídky mimo otevírací dobu.'),
-        SizedBox(
-          height: kDMarginLarger,
-        ),
-        Center(
-          child: Container(
-            width: double.infinity,
-            child: MyButton(
-              textOfButton: 'Přejít na stránku památky',
-              onPressed: () {
-                Navigator.pushNamed(context, MonumentsRosaCoeli.id);
-              },
-            ),
-          ),
-        ),
-        SizedBox(
-          height: kDMargin,
-        ),
-        Center(
-          child: Container(
-            width: double.infinity,
-            child: MyButton(
-              textOfButton:
-                  'Informace o vstupném, otevírací době a dalších naleznete zde',
-              onPressed: () async {
-                //po stisknutí otevře stránku v externím prohlížeči
-                OpenUrlInBrowser().openUrl(RosaCoeli().url);
-              },
-            ),
-          ),
-        ),
-        SizedBox(
-          height: kDMarginLarger,
-        ),
-        SizedBox(height: kDMarginLarger),
-        TextDefaultStandartBold(text: '3. Hrad a zámek \(cca 75 minut\)'),
-        SizedBox(height: kDMargin),
-        ImageOnPageWithOnTap(
-          assetImage: CastleAndChateau().imageGallery[0],
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => SinglePhotoViewPage(
-                    assetImage: CastleAndChateau().imageGallery[0]),
-              ),
-            );
-          },
-        ),
-        SizedBox(
-          height: kDMarginLarger,
-        ),
-        TextDefaultStandart(
-            text:
-                'Prohlídka s komentářem průvodce\n\nMožnost soukromé prohlídky mimo otevírací dobu.'),
-        SizedBox(
-          height: kDMarginLarger,
-        ),
-        Center(
-          child: Container(
-            width: double.infinity,
-            child: MyButton(
-              textOfButton: 'Přejít na stránku památky',
-              onPressed: () {
-                Navigator.pushNamed(context, CastleAndChateau().id);
-              },
-            ),
-          ),
-        ),
-        SizedBox(
-          height: kDMargin,
-        ),
-        Center(
-          child: Container(
-            width: double.infinity,
-            child: MyButton(
-              textOfButton:
-                  'Informace o vstupném, otevírací době a dalších naleznete zde',
-              onPressed: () async {
-                //po stisknutí otevře stránku v externím prohlížeči
-                OpenUrlInBrowser().openUrl(CastleAndChateau().url);
-              },
-            ),
-          ),
-        ),
-        SizedBox(
-          height: kDMarginLarger,
-        ),
+        DividerText(),
+        CardOfSubject(
+            name: 'Schůzka 7.13',
+            phoneNumber: '777 008 713',
+            webPage: 'www.schuzka7-13.cz',
+            webPageURL: 'www.schuzka7-13.cz',
+            address: 'Tovární 3',
+            addressURL:
+                'http://www.google.com/maps?q=49.0702896,16.466907&z=14&t=m',
+            label: 'Platba kartou, pivo, víno, občerstvení, zahrádka'),
+        DividerText(),
+        CardOfSubject(
+            name: 'Pizzerie u Ježků',
+            phoneNumber: '602 621 361',
+            webPage: 'www.pizzaujezku.cz',
+            webPageURL: 'https://www.pizzaujezku.cz/',
+            address: 'Náměstí míru 5',
+            addressURL:
+            'https://goo.gl/maps/9HNrgyW4a3gSzkZ88',
+            label: ''),
+        DividerText(),
+        CardOfSubject(
+            name: 'Restaurace Prince',
+            phoneNumber: '721 273 180',
+            webPage: 'Facebook',
+            webPageURL: 'https://www.facebook.com/Restaurace-Prince-708638542834623/',
+            address: 'Náměstí míru 2',
+            addressURL:
+            'https://goo.gl/maps/g2DiYxCvN8C9Z6Hp6',
+            label: ''),
+        DividerText(),
+        CardOfSubject(
+            name: 'Restaurace u Gottwaldů',
+            phoneNumber: '604 675 002',
+            webPage: 'web',
+            webPageURL: 'http://web.quick.cz/restauraceugottwaldu/',
+            address: 'Náměstí míru 22',
+            addressURL:
+            'https://goo.gl/maps/g2DiYxCvN8C9Z6Hp6',
+            label: 'Platba kartou, pivo, víno, občerstvení, zahrádka'),
       ],
     );
   }
 }
-
