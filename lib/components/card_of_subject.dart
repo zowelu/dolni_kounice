3925d867adddb40d93f6b4ce7a32cf647cb6da74
zoublu <zoublu@email.cz>
2021-07-12 01:18:43 +0200

    vytvoření souboru a tříd cardOfSubject

---
+++ lib/components/card_of_subject.dart
@@ -0,0 +1,95 @@
+import 'package:dolni_kounice/components/open_phone_url.dart';
+import 'package:dolni_kounice/components/text_default_standart.dart';
+import 'package:flutter/material.dart';
+import 'package:dolni_kounice/constants/constants.dart';
+import 'package:dolni_kounice/components/open_url_in_browser.dart';
+import 'package:dolni_kounice/components/my_button.dart';
+
+class CardOfSubject extends StatelessWidget {
+  const CardOfSubject(
+      {@required this.name,
+        @required this.phoneNumber,
+        @required this.webPage,
+        @required this.webPageURL,
+        @required this.address,
+        @required this.addressURL,
+        @required this.label});
+  final String name;
+  final String phoneNumber;
+  final String webPage;
+  final String webPageURL;
+  final String address;
+  final String addressURL;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.center,
+          child: Text(
+            name,
+            style: TextStyle(
+                decoration: TextDecoration.underline,
+                decorationColor: kDividerColor,
+                decorationThickness: 3.0,
+                color: Colors.white,
+                fontSize: kDFontSizeHeader),
+            //textAlign: TextAlign.start,
+          ),
+        ),
+        SizedBox(height: kDMarginLarger),
+        Row(
+          children: [
+            TextDefaultStandart(text: 'Telefon:'),
+            SizedBox(
+              width: 20,
+            ),
+            MyButton(
+              textOfButton: phoneNumber,
+              onPressed: () async {
+                //po stisknutí otevře dialer pro volání
+                OpenPhoneURL().makePhoneCall(phoneNumber);
+              },
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            TextDefaultStandart(text: 'Web:'),
+            SizedBox(
+              width: 20,
+            ),
+            MyButton(
+              textOfButton: webPage,
+              onPressed: () async {
+                //po stisknutí otevře stránku v externím prohlížeči
+                OpenUrlInBrowser().openUrl(webPageURL);
+              },
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            TextDefaultStandart(text: 'Adresa:'),
+            SizedBox(
+              width: 20,
+            ),
+            MyButton(
+              textOfButton: address,
+              onPressed: () async {
+                //po stisknutí otevře stránku v externím prohlížeči
+                OpenUrlInBrowser().openUrl(addressURL);
+              },
+            ),
+          ],
+        ),
+        SizedBox(height: kDMarginLarger),
+        TextDefaultStandart(
+          text: label,
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/services/coffeeAndRestaurant.dart
+++ lib/screens/services/coffeeAndRestaurant.dart
@@ -1,7 +1,6 @@
 import 'package:dolni_kounice/components/image_on_page_with_ontap.dart';
 import 'package:dolni_kounice/components/monuments/castle_and_chateau.dart';
 import 'package:dolni_kounice/components/monuments/rosa_coeli.dart';
-import 'package:dolni_kounice/components/open_phone_url.dart';
 import 'package:dolni_kounice/components/text_default_standart.dart';
 import 'package:dolni_kounice/components/text_default_standart_bold.dart';
 import 'package:dolni_kounice/screens/monuments/monuments_rosa_coeli.dart';
@@ -13,6 +12,7 @@ import 'package:dolni_kounice/components/my_button.dart';
 import 'package:dolni_kounice/components/monuments/jevish_synagogue.dart';
 import 'package:dolni_kounice/components/single_photo_view_page.dart';
 import 'package:dolni_kounice/screens/monuments/monuments_jevish_synagogue.dart';
+import 'package:dolni_kounice/components/card_of_subject.dart';
 
 class CoffeeAndRestaurant extends StatelessWidget {
   static String id = '/cofeeAndRestaurant';
@@ -235,91 +235,3 @@ class CoffeeAndRestaurant extends StatelessWidget {
   }
 }
 
-class CardOfSubject extends StatelessWidget {
-  const CardOfSubject(
-      {@required this.name,
-      @required this.phoneNumber,
-      @required this.webPage,
-      @required this.webPageURL,
-      @required this.address,
-      @required this.addressURL,
-      @required this.label});
-  final String name;
-  final String phoneNumber;
-  final String webPage;
-  final String webPageURL;
-  final String address;
-  final String addressURL;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Align(
-          alignment: Alignment.center,
-          child: Text(
-            name,
-            style: TextStyle(
-                decoration: TextDecoration.underline,
-                decorationColor: kDividerColor,
-                decorationThickness: 3.0,
-                color: Colors.white,
-                fontSize: kDFontSizeHeader),
-            //textAlign: TextAlign.start,
-          ),
-        ),
-        SizedBox(height: kDMarginLarger),
-        Row(
-          children: [
-            TextDefaultStandart(text: 'Telefon:'),
-            SizedBox(
-              width: 20,
-            ),
-            MyButton(
-              textOfButton: phoneNumber,
-              onPressed: () async {
-                //po stisknutí otevře dialer pro volání
-                OpenPhoneURL().makePhoneCall(phoneNumber);
-              },
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            TextDefaultStandart(text: 'Web:'),
-            SizedBox(
-              width: 20,
-            ),
-            MyButton(
-              textOfButton: webPage,
-              onPressed: () async {
-                //po stisknutí otevře stránku v externím prohlížeči
-                OpenUrlInBrowser().openUrl(webPageURL);
-              },
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            TextDefaultStandart(text: 'Adresa:'),
-            SizedBox(
-              width: 20,
-            ),
-            MyButton(
-              textOfButton: address,
-              onPressed: () async {
-                //po stisknutí otevře stránku v externím prohlížeči
-                OpenUrlInBrowser().openUrl(addressURL);
-              },
-            ),
-          ],
-        ),
-        SizedBox(height: kDMarginLarger),
-        TextDefaultStandart(
-          text: label,
-        ),
-      ],
-    );
-  }
-}
