e8a442979a5c8edbe232d0b08cdd1f5ec46a8bde
zoublu <zoublu@email.cz>
2021-07-12 03:04:53 +0200

    přidán isLabel do coffeeAndRestaurant.dart

--- lib/screens/services/coffeeAndRestaurant.dart
+++ lib/screens/services/coffeeAndRestaurant.dart
@@ -29,6 +29,7 @@ class CoffeeAndRestaurant extends StatelessWidget {
           address: 'Hlavní 6',
           addressURL:
               'http://www.google.com/maps?q=49.0694522,16.4662409&z=14&t=m',
+          isLabel: true,
           label:
               'Platba kartou, Se psem, Bezbariérové, Dětský koutek, Zahrádka',
           isImage: false,
@@ -43,6 +44,7 @@ class CoffeeAndRestaurant extends StatelessWidget {
           address: 'Masarykovo náměstí 5',
           addressURL:
               'http://www.google.com/maps?q=49.0700406,16.464826&z=14&t=m',
+          isLabel: true,
           label: 'Platba kartou, Denní menu, Zahrádka',
           isImage: false,
           image: null,
@@ -56,6 +58,7 @@ class CoffeeAndRestaurant extends StatelessWidget {
           address: 'Tovární 3',
           addressURL:
               'http://www.google.com/maps?q=49.0702896,16.466907&z=14&t=m',
+          isLabel: true,
           label: 'Platba kartou, pivo, víno, občerstvení, zahrádka',
           isImage: false,
 
@@ -68,6 +71,7 @@ class CoffeeAndRestaurant extends StatelessWidget {
           webPageURL: 'https://www.pizzaujezku.cz/',
           address: 'Náměstí míru 5',
           addressURL: 'https://goo.gl/maps/9HNrgyW4a3gSzkZ88',
+          isLabel: false,
           label: '',
           isImage: false,
 
@@ -81,6 +85,7 @@ class CoffeeAndRestaurant extends StatelessWidget {
               'https://www.facebook.com/Restaurace-Prince-708638542834623/',
           address: 'Náměstí míru 2',
           addressURL: 'https://goo.gl/maps/g2DiYxCvN8C9Z6Hp6',
+          isLabel: false,
           label: '',
           isImage: false,
 
@@ -93,6 +98,7 @@ class CoffeeAndRestaurant extends StatelessWidget {
           webPageURL: 'http://web.quick.cz/restauraceugottwaldu/',
           address: 'Náměstí míru 22',
           addressURL: 'https://goo.gl/maps/g2DiYxCvN8C9Z6Hp6',
+          isLabel: true,
           label: 'Platba kartou, pivo, víno, občerstvení, zahrádka',
           isImage: false,
 
