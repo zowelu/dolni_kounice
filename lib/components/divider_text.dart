63e566172e467417e8272e19693d9888b04707cf
zoublu <zoublu@email.cz>
2021-07-12 03:05:18 +0200

    upraven margin na kdmargin

--- lib/components/divider_text.dart
+++ lib/components/divider_text.dart
@@ -11,7 +11,7 @@ class DividerText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(height: kDMarginLarger),
+        SizedBox(height: kDMargin),
         Divider(
           height: 5,
           color: kDividerColor,
