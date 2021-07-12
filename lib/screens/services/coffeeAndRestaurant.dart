0d07bb5a38f08a4676c62b1ab40278249602ade5
zoublu <zoublu@email.cz>
2021-07-12 02:42:56 +0200

    dotvoření coffeeAndRestaurant.dart

--- lib/screens/services/coffeeAndRestaurant.dart
+++ lib/screens/services/coffeeAndRestaurant.dart
@@ -31,57 +31,72 @@ class CoffeeAndRestaurant extends StatelessWidget {
               'http://www.google.com/maps?q=49.0694522,16.4662409&z=14&t=m',
           label:
               'Platba kartou, Se psem, Bezbariérové, Dětský koutek, Zahrádka',
+          isImage: false,
+
         ),
         DividerText(),
         CardOfSubject(
-            name: 'Hotel Vinum coeli',
-            phoneNumber: '730 825 882',
-            webPage: 'www.vinumcoeli.cz',
-            webPageURL: 'http://www.vinumcoeli.cz/',
-            address: 'Masarykovo náměstí 5',
-            addressURL:
-                'http://www.google.com/maps?q=49.0700406,16.464826&z=14&t=m',
-            label: 'Platba kartou, Denní menu, Zahrádka'),
+          name: 'Hotel Vinum coeli',
+          phoneNumber: '730 825 882',
+          webPage: 'www.vinumcoeli.cz',
+          webPageURL: 'http://www.vinumcoeli.cz/',
+          address: 'Masarykovo náměstí 5',
+          addressURL:
+              'http://www.google.com/maps?q=49.0700406,16.464826&z=14&t=m',
+          label: 'Platba kartou, Denní menu, Zahrádka',
+          isImage: false,
+          image: null,
+        ),
         DividerText(),
         CardOfSubject(
-            name: 'Schůzka 7.13',
-            phoneNumber: '777 008 713',
-            webPage: 'www.schuzka7-13.cz',
-            webPageURL: 'www.schuzka7-13.cz',
-            address: 'Tovární 3',
-            addressURL:
-                'http://www.google.com/maps?q=49.0702896,16.466907&z=14&t=m',
-            label: 'Platba kartou, pivo, víno, občerstvení, zahrádka'),
+          name: 'Schůzka 7.13',
+          phoneNumber: '777 008 713',
+          webPage: 'www.schuzka7-13.cz',
+          webPageURL: 'www.schuzka7-13.cz',
+          address: 'Tovární 3',
+          addressURL:
+              'http://www.google.com/maps?q=49.0702896,16.466907&z=14&t=m',
+          label: 'Platba kartou, pivo, víno, občerstvení, zahrádka',
+          isImage: false,
+
+        ),
         DividerText(),
         CardOfSubject(
-            name: 'Pizzerie u Ježků',
-            phoneNumber: '602 621 361',
-            webPage: 'www.pizzaujezku.cz',
-            webPageURL: 'https://www.pizzaujezku.cz/',
-            address: 'Náměstí míru 5',
-            addressURL:
-            'https://goo.gl/maps/9HNrgyW4a3gSzkZ88',
-            label: ''),
+          name: 'Pizzerie u Ježků',
+          phoneNumber: '602 621 361',
+          webPage: 'www.pizzaujezku.cz',
+          webPageURL: 'https://www.pizzaujezku.cz/',
+          address: 'Náměstí míru 5',
+          addressURL: 'https://goo.gl/maps/9HNrgyW4a3gSzkZ88',
+          label: '',
+          isImage: false,
+
+        ),
         DividerText(),
         CardOfSubject(
-            name: 'Restaurace Prince',
-            phoneNumber: '721 273 180',
-            webPage: 'Facebook',
-            webPageURL: 'https://www.facebook.com/Restaurace-Prince-708638542834623/',
-            address: 'Náměstí míru 2',
-            addressURL:
-            'https://goo.gl/maps/g2DiYxCvN8C9Z6Hp6',
-            label: ''),
+          name: 'Restaurace Prince',
+          phoneNumber: '721 273 180',
+          webPage: 'Facebook',
+          webPageURL:
+              'https://www.facebook.com/Restaurace-Prince-708638542834623/',
+          address: 'Náměstí míru 2',
+          addressURL: 'https://goo.gl/maps/g2DiYxCvN8C9Z6Hp6',
+          label: '',
+          isImage: false,
+
+        ),
         DividerText(),
         CardOfSubject(
-            name: 'Restaurace u Gottwaldů',
-            phoneNumber: '604 675 002',
-            webPage: 'web',
-            webPageURL: 'http://web.quick.cz/restauraceugottwaldu/',
-            address: 'Náměstí míru 22',
-            addressURL:
-            'https://goo.gl/maps/g2DiYxCvN8C9Z6Hp6',
-            label: 'Platba kartou, pivo, víno, občerstvení, zahrádka'),
+          name: 'Restaurace u Gottwaldů',
+          phoneNumber: '604 675 002',
+          webPage: 'web',
+          webPageURL: 'http://web.quick.cz/restauraceugottwaldu/',
+          address: 'Náměstí míru 22',
+          addressURL: 'https://goo.gl/maps/g2DiYxCvN8C9Z6Hp6',
+          label: 'Platba kartou, pivo, víno, občerstvení, zahrádka',
+          isImage: false,
+
+        ),
       ],
     );
   }
