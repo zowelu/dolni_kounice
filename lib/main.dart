068b756422eeb793527d36f6807fe04e0c3f986b
zoublu <zoublu@email.cz>
2021-08-11 02:56:19 +0200

    opravena cesta k souboru do složky audio_guides pro audio_guide jevissh synagogue

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:dolni_kounice/screens/audio_guides/audio_guide_chapel_of_st_antonine/audio_guide_chapel_of_st_antonine.dart';
 import 'package:dolni_kounice/screens/audio_guides/audio_guide_chapel_of_st_antonine/audio_guide_chapel_of_st_antonine_1.dart';
-import 'package:dolni_kounice/screens/audio_guide_jevish_synagogue/audio_guide_jevish_synagogue.dart';
+import 'package:dolni_kounice/screens/audio_guides/audio_guide_jevish_synagogue/audio_guide_jevish_synagogue.dart';
 import 'package:dolni_kounice/screens/monuments/monuments_castle_and_chateau.dart';
 import 'package:dolni_kounice/screens/monuments/monuments_chapel_of_st_antonine.dart';
 import 'package:dolni_kounice/screens/monuments/monuments_chapel_of_st_john_the_baptist.dart';
